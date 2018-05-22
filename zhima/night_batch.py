f59b84969c07cebcda40e82c529bced9cf898f96
eric <eric.gibert@yahoo.fr>
2018-05-22 22:34:57 +0800

    night batch

---
+++ zhima/night_batch.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+"""
+Executed every night at 0:01AM
+
+- for each user:
+    - check if we need a reminder 7 days before expiration
+    - check if we need a reminder when expiration is reached + change status to NOT_OK
+
+"""
+__author__ = "Eric Gibert"
+__version__ = "1.0.20180520 Dong Bei"
+__email__ =  "ericgibert@yahoo.fr"
+__license__ = "MIT"
+
+from datetime import date
+from member import Member
+from send_email import send_email
+
+db = Member(debug=True)
+
+for m in db.select(columns="id", one_only=False):
+    member = Member(id=m['id'])
+    print(member, member['status'])
+    if member['status'] != "OK": continue
+    if (member['validity'] - date.today()).days == 7:
+        print("7 days!")
+        if member['email']:
+            send_email(
+                "Your membership to Xin Che Jian will expire in one week",
+                db.mailbox["username"],
+                (member['email'], ),
+                """
+Dear member,
+
+This message to inform you that we noticed that your membership to Xin Che Jian hackerspace will expire on {}.
+
+Please approach one of our staff during your next visit to renew it.
+
+Looking forward seeing you @ XCJ!
+                """.format(member['validity']),
+                """
+<h3>Dear member,</h3>
+<p>
+This message to inform you that we noticed that your membership to Xin Che Jian hackerspace will expire on <b>{}</b>.
+<br>
+Please approach one of our staff during your next visit to renew it.
+<br>
+Looking forward seeing you @ XCJ!
+</p>
+    """.format(member['validity']),
+                [],
+                db.mailbox["server"], db.mailbox['port'],
+                db.mailbox["username"], db.mailbox['password'],
+                debug=0
+        )
+    elif date.today() > member['validity']:
+        print("Expired!")
+        member.update(id=member.id, status="NOT_OK")
+        if member['email']:
+            send_email(
+                "Your membership to Xin Che Jian has expired",
+                db.mailbox["username"],
+                (member['email'], ),
+                """
+Dear member,
+
+This message to inform you that your membership to Xin Che Jian hackerspace has expired today.
+
+Please approach one of our staff during your next visit to renew it.
+
+Looking forward seeing you @ XCJ!""",
+                """
+<h3>Dear member,</h3>
+<p>
+This message to inform you that your membership to Xin Che Jian hackerspace has expired today.
+<br>
+Please approach one of our staff during your next visit to renew it.
+<br>
+Looking forward seeing you @ XCJ!
+</p>""",
+                [],
+                db.mailbox["server"], db.mailbox['port'],
+                db.mailbox["username"], db.mailbox['password'],
+                debug=0
+            )
