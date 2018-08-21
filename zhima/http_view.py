a833f05825e68816893657432ecf3e84d7cf1d17
Eric Gibert <ericgibert@yahoo.fr>
2018-08-21 19:21:32 +0800

    beaker addition

--- zhima/http_view.py
+++ zhima/http_view.py
@@ -49,7 +49,7 @@ class Session_Manager():
     def __init__(self):
         self.session = {}
     def get_session(self):
-        self.session = request.environ.get('beaker.session')
+        self.session = request.environ.get('beaker.session') or {}
     def save(self, session):
         for k, v in session:
             self.session[k] = v
